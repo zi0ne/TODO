166aca735473a8a6a4052229ac9968904e2ee00c
zi0ne <jiwon0331j@gmail.com>
2023-04-08 19:17:47 +0900

    state 관리, 컴포넌트 합치기

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TodoList, { Todo } from './components/TodoList';
 
-function App() {
+//Todo 상태관리
+export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+ 
+
+
+  // 문자열 타입 text를 인자로 받아 newTo 객체로 만들어 setTodos를 사용하여 todo에 추가
+  const handleAddTodo = (text: string) => {
+    const newTodo: Todo = {
+      id: todos.length + 1,
+      text: text,
+      done: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  //number 타입 id를 인자로 받아 바뀐 done값을 업데이트하여 상태에 반영
+  const handleTodoToggle = (id: number) => {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, done: !todo.done };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  
+  const handleDeleteTodo = (id: number) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoList todos={todos} onToggleDone={handleTodoToggle} onDeleteTodo={handleDeleteTodo} />
     </div>
   );
-}
+};
 
-export default App;
